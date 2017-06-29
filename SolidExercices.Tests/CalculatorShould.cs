783a6244fc0552b4038554f645b997134ec65638
p0904294 <skhelifa1@gmail.com>
2017-06-29 16:38:52 +0200

    Vérifiaction exception bien générer lorsqu'on a plus de 3 operateur

--- SolidExercices.Tests/CalculatorShould.cs
+++ SolidExercices.Tests/CalculatorShould.cs
@@ -1,4 +1,5 @@
-﻿using NFluent;
+﻿using System;
+using NFluent;
 using NUnit.Framework;
 
 namespace SolidExercices.Tests
@@ -11,6 +12,7 @@ namespace SolidExercices.Tests
             var calculator = new Calculator();
             var result = calculator.Calculate("1+2,3");
             Check.That(result).IsEqualTo(3.3);
+            Check.ThatCode(() => calculator.Calculate("3+1,5+3")).Throws<ArgumentException>();
         }
         public void CalculateADivide()
         {
@@ -23,13 +25,14 @@ namespace SolidExercices.Tests
             var calculator = new Calculator();
             var result = calculator.Calculate("2x3");
             Check.That(result).IsEqualTo(6);
-
         }
         public void CalculateASubstract()
         {
             var calculator = new Calculator();
             var result = calculator.Calculate("3-1,5");
+
             Check.That(result).IsEqualTo(1.5);
+            Check.ThatCode(() => calculator.Calculate("3-1,5-3")).Throws<ArgumentException>();
         }
     }
 }
