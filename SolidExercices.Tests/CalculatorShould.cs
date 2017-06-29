12af2bd68a02b9db7f05c9cdc9a84264a23213f2
p0904294 <skhelifa1@gmail.com>
2017-06-29 14:37:47 +0200

    Mise en place des tests pour les differentes operations

--- SolidExercices.Tests/CalculatorShould.cs
+++ SolidExercices.Tests/CalculatorShould.cs
@@ -12,5 +12,23 @@ namespace SolidExercices.Tests
             var result = calculator.Calculate("1+2,3");
             Check.That(result).IsEqualTo(3.3);
         }
+        public void CalculateADivide()
+        {
+            var calculator = new Calculator();
+            var result = calculator.Calculate("3/2");
+            Check.That(result).IsEqualTo(1.5);
+        }
+        public void CalculateAMultiply()
+        {
+            var calculator = new Calculator();
+            var result = calculator.Calculate("2x3");
+            Check.That(result).IsEqualTo(6);
+        }
+        public void CalculateASubstract()
+        {
+            var calculator = new Calculator();
+            var result = calculator.Calculate("3-1,5");
+            Check.That(result).IsEqualTo(1.5);
+        }
     }
 }
--- SolidExercices/Calculator.cs
+++ SolidExercices/Calculator.cs
@@ -7,6 +7,28 @@ namespace SolidExercices
     {
         public double Calculate(string operation)
         {
+
+
+            double result = 0; 
+            string[] substrings = operation.Split('+','-','/','x');
+            if (operation.Contains("+"))
+            {
+                result = Convert.ToDouble(substrings.GetValue(0)) + Convert.ToDouble(substrings.GetValue(1));
+            }
+            else if (operation.Contains("/"))
+            {
+                result = Convert.ToDouble(substrings.GetValue(0)) / Convert.ToDouble(substrings.GetValue(1));
+            }
+            else if (operation.Contains("x"))
+            {
+                result = Convert.ToDouble(substrings.GetValue(0)) * Convert.ToDouble(substrings.GetValue(1));
+            }
+            else if (operation.Contains("/"))
+            {
+                result = Convert.ToDouble(substrings.GetValue(0)) / Convert.ToDouble(substrings.GetValue(1));
+            }
+            return result;
+ 
             throw new NotImplementedException();
         }
     }
