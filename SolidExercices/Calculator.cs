6c96ed426097c8f35d8eb1fe14cd460df3f38893
p0904294 <skhelifa1@gmail.com>
2017-06-29 14:48:32 +0200

    Mise en place d'une exception si l'opération contient plus de ddeux opérateur

--- SolidExercices/Calculator.cs
+++ SolidExercices/Calculator.cs
@@ -9,27 +9,34 @@ namespace SolidExercices
         {
 
 
-            double result = 0; 
-            string[] substrings = operation.Split('+','-','/','x');
-            if (operation.Contains("+"))
-            {
-                result = Convert.ToDouble(substrings.GetValue(0)) + Convert.ToDouble(substrings.GetValue(1));
-            }
-            else if (operation.Contains("/"))
-            {
-                result = Convert.ToDouble(substrings.GetValue(0)) / Convert.ToDouble(substrings.GetValue(1));
-            }
-            else if (operation.Contains("x"))
-            {
-                result = Convert.ToDouble(substrings.GetValue(0)) * Convert.ToDouble(substrings.GetValue(1));
-            }
-            else if (operation.Contains("/"))
-            {
-                result = Convert.ToDouble(substrings.GetValue(0)) / Convert.ToDouble(substrings.GetValue(1));
-            }
-            return result;
- 
-            throw new NotImplementedException();
+            
+            
+                double result = 0;
+                string[] substrings = operation.Split('+', '-', '/', 'x');
+                if (substrings.Length == 2)
+                {
+                    Console.WriteLine("{0} n'est pas une opération à deux opérateur", operation);
+                }
+                if (operation.Contains("+"))
+                {
+                    result = Convert.ToDouble(substrings.GetValue(0)) + Convert.ToDouble(substrings.GetValue(1));
+                }
+                else if (operation.Contains("/"))
+                {
+                    result = Convert.ToDouble(substrings.GetValue(0)) / Convert.ToDouble(substrings.GetValue(1));
+                }
+                else if (operation.Contains("x"))
+                {
+                    result = Convert.ToDouble(substrings.GetValue(0)) * Convert.ToDouble(substrings.GetValue(1));
+                }
+                else if (operation.Contains("/"))
+                {
+                    result = Convert.ToDouble(substrings.GetValue(0)) / Convert.ToDouble(substrings.GetValue(1));
+                }
+                return result;
+
+
+
         }
     }
 }
